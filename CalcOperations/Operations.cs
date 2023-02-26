e0347026982700ae347c5233e45486c2218fc43e
Gary <Gary.Dix@enform.nz>
2023-02-27 11:23:32 +1300

    Added Unit Tests

--- CalcOperations/Operations.cs
+++ CalcOperations/Operations.cs
@@ -1,63 +1,57 @@
-﻿namespace Calculator_with_tests.CalcOperations
-{
-    public class Operations
-    {
+﻿using Calculator_with_tests.Model;
 
-
-        public string Add(string Num1, string Num2)
-        {
+namespace Calculator_with_tests.CalcOperations {
+    public class Operations {
+        public string Add(string Num1, string Num2) {
             float num1 = Convert.ToSingle(Num1);
             float num2 = Convert.ToSingle(Num2);
             float answer = num1 + num2;
             return (Num1 + " + " + Num2 + " = " + answer.ToString()); //send back the answer
         }
-
-
-        public string Subtract(string Num1, string Num2)
-        {
+        public string Subtract(string Num1, string Num2) {
             float num1 = Convert.ToSingle(Num1);
             float num2 = Convert.ToSingle(Num2);
             float answer = num1 - num2;
             return num1.ToString() + " - " + num2.ToString() + " = " + answer.ToString();
         }
-
-
-        public string Divide(string Num1, string Num2)
-        {
+        public string Divide(string Num1, string Num2) {
             float num1 = Convert.ToSingle(Num1);
             float num2 = Convert.ToSingle(Num2);
             float answer = num1 / num2;
             return (Num1 + " / " + Num2 + " = " + answer.ToString()); //send back the answer
         }
-
-
-
-        public string Multiply(string Num1, string Num2)
-        {
+        public string Multiply(string Num1, string Num2) {
             float num1;
             float num2;
             float answer;
 
-            try
-            {
+            try {
                 num1 = Convert.ToSingle(Num1);
                 num2 = Convert.ToSingle(Num2);
                 answer = num1 * num2;
             }
-            catch (Exception)
-            {
+            catch (Exception) {
 
                 return "Error";
             }
-
-
             return (Num1 + " * " + Num2 + " = " + answer.ToString()); //send back the answer
         }
 
+        public bool IsNumberOdd(int value) {
+            //return true if you can divide the value by 2 and get a remainder of 1
+            return value % 2 == 1;
+        }
 
 
+        public Double AddTheory(double x, double y) {
+            return x + y;
+        }
 
+        public double AddClassFact(CalcData calcData) {
 
+            calcData.Answer = calcData.num1 + calcData.num2;
+            return calcData.Answer;
+        }
 
     }
 }
---
+++ Model/CalcData.cs
@@ -0,0 +1,7 @@
+﻿namespace Calculator_with_tests.Model {
+    public class CalcData {
+        public double num1 { get; set; }
+        public double num2 { get; set; }
+        public double Answer { get; set; }
+    }
+}
